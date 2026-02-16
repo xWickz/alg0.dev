5e249a1ab618deb69c408bccb3a013a9ab819ee8
midudev <miduga@gmail.com>
2026-02-16 12:29:15 +0100

    Enhance Astro configuration with site details, React integration, i18n support, and Tailwind CSS plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@astrojs/react'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://alg0.dev',
+  integrations: [react()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
